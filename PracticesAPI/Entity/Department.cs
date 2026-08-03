a15159d744ced253da5aadac9755a5ce47af98c7
Mayur Jade <jademayur@gmail.com>
2026-08-03 17:20:01 +0530

    create model class for emp and dept

---
+++ PracticesAPI/Entity/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticesAPI.Entity
+{
+    public class Department
+    {
+        [Key]
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+    }
+}
---
+++ PracticesAPI/Entity/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticesAPI.Entity
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int DeptId { get; set; }
+        public string? mobile { get; set; }
+        public string? Email { get; set; }
+        public decimal Salary { get; set; } = 0;
+
+        public Department? Department { get; set; }
+    }
+}

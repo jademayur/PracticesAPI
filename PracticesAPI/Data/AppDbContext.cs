857ccad7f9cde54263aa31bab99042af27837f98
Mayur Jade <jademayur@gmail.com>
2026-08-03 17:27:58 +0530

    create context file

---
+++ PracticesAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using PracticesAPI.Entity;
+
+namespace PracticesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}

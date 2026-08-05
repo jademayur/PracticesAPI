ce0fe0b282c6fd0ce47f93561dea1f5c65e190a4
Mayur Jade <jademayur@gmail.com>
2026-08-05 15:25:03 +0530

    create controller

---
+++ PracticesAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PracticesAPI.DTOs;
+using PracticesAPI.Interface;
+
+namespace PracticesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _employeeService.GetAllEmployeesAsync();
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id) {
+            var employee = await _employeeService.GetEmployeeByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] CreateEmpDto employeeDto)
+        {
+            var createdEmployee = await _employeeService.CreateEmployeeAsync(employeeDto);
+            return CreatedAtAction(nameof(GetEmployee), new { id = createdEmployee.EmpId }, createdEmployee);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, [FromBody] UpdateEmpDto employeeDto)
+        {
+            var result = await _employeeService.UpdateEmployeeAsync(id, employeeDto);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var result = await _employeeService.DeleteEmployeeAsync(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
--- PracticesAPI/Program.cs
+++ PracticesAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PracticesAPI.Data;
+using PracticesAPI.Interface;
+using PracticesAPI.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
 var app = builder.Build();
 

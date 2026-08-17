0429004f1a65a485c2d26dbeabec069613f2af6d
Mayur Jade <jademayur@gmail.com>
2026-08-17 17:18:36 +0530

    change in interface and service

--- PracticesAPI/Interface/IDepartment.cs
+++ PracticesAPI/Interface/IDepartment.cs
@@ -4,10 +4,6 @@ namespace PracticesAPI.Interface
 {
     public interface IDepartment
     {
-        Task<IEnumerable<DepartmentDto>> GetAllDepartmentsAsync();
-        Task<DepartmentDto> GetDepartmentsAsync(int deptId);
-        Task<DepartmentDto> CreateDepartmentAsync(DepartmentDto department);
-        Task<bool> UpdateDepartmentAsync(int deptId, DepartmentDto department);
-        Task<bool> DeleteDepartmentAsync(int deptId);
+      
     }
 }

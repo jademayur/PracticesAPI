9d71df55f9bf43d2acf664feb7c221108f94cbaf
Mayur Jade <jademayur@gmail.com>
2026-08-17 17:18:21 +0530

    change in controller

--- PracticesAPI/Controllers/DepartmentController.cs
+++ PracticesAPI/Controllers/DepartmentController.cs
@@ -9,57 +9,6 @@ namespace PracticesAPI.Controllers
     [ApiController]
     public class DepartmentController : ControllerBase
     {
-        private readonly IDepartment _departmentService;
-        public DepartmentController(IDepartment departmentService)
-        {
-            _departmentService = departmentService;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetAllDepartments()
-        {
-            var departments = await _departmentService.GetAllDepartmentsAsync();
-            return Ok(departments);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetDepartment(int id)
-        {
-            var department = await _departmentService.GetDepartmentsAsync(id);
-            if (department == null)
-            {
-                return NotFound();
-            }
-            return Ok(department);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateDepartment([FromBody] DepartmentDto departmentDto)
-        {
-            var createdDepartment = await _departmentService.CreateDepartmentAsync(departmentDto);
-            return CreatedAtAction(nameof(GetDepartment), new { id = createdDepartment.DeptId }, createdDepartment);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateDepartment(int id, [FromBody] DepartmentDto departmentDto)
-        {
-            var updated = await _departmentService.UpdateDepartmentAsync(id, departmentDto);
-            if (!updated)
-            {
-                return NotFound();
-            }
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteDepartment(int id)
-        {
-            var deleted = await _departmentService.DeleteDepartmentAsync(id);
-            if (!deleted)
-            {
-                return NotFound();
-            }
-            return NoContent();
-        }
+       
     }
 }

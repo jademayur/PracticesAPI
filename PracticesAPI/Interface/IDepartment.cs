f40a545b1352786695f3bbfa4708fc866519a812
Mayur Jade <jademayur@gmail.com>
2026-08-21 15:41:58 +0530

    create interface for dept

--- PracticesAPI/Interface/IDepartment.cs
+++ PracticesAPI/Interface/IDepartment.cs
@@ -4,6 +4,13 @@ namespace PracticesAPI.Interface
 {
     public interface IDepartment
     {
-      
+      Task<IEnumerable<DepartmentDto>> GetAllDepartmentsAsync();
+      Task<DepartmentDto> GetDepartmentByIdAsync(int deptId);
+      Task<DepartmentDto> CreateDepartmentAsync(CreateDeptDto departmentDto);
+      Task<bool> UpdateDepartmentAsync(int deptId, UpdateDeptDto departmentDto);
+      Task<bool> DeleteDepartmentAsync(int deptId);
+
+
+
     }
 }
